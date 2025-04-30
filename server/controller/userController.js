552c9027078fada066422c9ad252dae410a0eadd
YanbiPanjaitan <yanbi.2001@gmail.com>
2025-04-30 14:43:00 +0700

    feat: googleLogin

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -52,20 +52,30 @@ class UserController {
   static async googleLogin(req, res, next) {
     const client = new OAuth2Client();
     const {googleToken} = req.body;
+
     try {
+      if (!googleToken) {
+        throw {name: "BadRequest", message: "Google token is required"};
+      }
+
       const ticket = await client.verifyIdToken({
         idToken: googleToken,
         audience: process.env.GOOGLE_CLIENT_ID,
       });
+
       const payload = ticket.getPayload();
+      const username = payload.email.split("@")[0];
+
       const [user, created] = await User.findOrCreate({
         where: {email: payload.email},
         defaults: {
           email: payload.email,
+          username,
           password: Math.random().toString(36).slice(-8),
         },
         hooks: false,
       });
+
       const token = signToken({id: user.id});
       res.status(created ? 201 : 200).json({access_token: token});
     } catch (error) {
